0b1efc8a202deb462d34c09183f878a3c0262c33
christianalfoni <christian.jorgensen@marcello.no>
2013-09-09 09:28:44 +0200

    Added project files

---
+++ backbone-draganddrop-delegation.js
@@ -0,0 +1,90 @@
+(function ($) {
+    var event,
+        getEvent = function () {
+            return event || window.event;
+        },
+        setEvent = function (ev) {
+            event = ev;
+        };
+    // FireFox does not have a global event object.
+    // By capturing the mousedown event on the window
+    // we create our own event object
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') >= 0) {
+        window.addEventListener('mousedown', setEvent, true);
+    }
+    $.fn.extend({
+        detectDrag: function () {
+            var isDragging = false,
+                dragStart = null,
+                $clone = null,
+                $el = null,
+                data = {
+                    set: function (value) {
+                        data = value;
+                    }
+                },
+                $dragoverElement = null,
+                preventSelection = function (event) {
+                    event.preventDefault();
+                    return false;
+                },
+                dragDetected = function (event) {
+                    return (Math.abs(dragStart.clientX - event.clientX) > 5 || Math.abs(dragStart.clientY - event.clientY) > 5)
+                },
+                startDrag = function (event) {
+                    isDragging = true;
+                    $clone = $el.clone().css({
+                        position: 'absolute',
+                        left: event.clientX + 5,
+                        top: event.clientY + 5
+                    }).appendTo('body');
+                    // Will get the returned value of any dragstart
+                    // triggered event and set it as data
+                    $el.trigger('dragstart', [data, $clone, $el]);
+                },
+                drag = function (event) {
+                    var $target = $(event.target),
+                        isNewTarget = $target[0] !== ($dragoverElement ? $dragoverElement[0] : null);
+                    if ($target.attr('dropable') && isNewTarget) {
+                        $dragoverElement = $target;
+                        $target.trigger('dragenter', [$clone, $el]);
+                    } else if ($dragoverElement && isNewTarget) {
+                        $dragoverElement.trigger('dragleave', [$clone, $el]);
+                        $dragoverElement = null;
+                    }
+                    $clone.css({
+                        left: event.clientX + 5,
+                        top: event.clientY + 5
+                    });
+                },
+                handleDrag = function (event) {
+                    if (isDragging) {
+                        drag(event);
+                    } else if (dragDetected(event)) {
+                        startDrag(event);
+                    }
+                },
+                stopDrag = function (event) {
+                    var $target = $(event.target),
+                        $window = $(window);
+                    if (isDragging) {
+                        $clone.remove();
+                        if ($target.attr('dropable')) {
+                            $target.trigger('drop', [data, $clone, $el]);
+                        } else {
+                            $el.trigger('dragend', [$clone, $el]);
+                        }
+                    }
+                    isDragging = false;
+                    $el.off('selectstart', preventSelection);
+                    $('body').css('user-select', 'auto');
+                    $window.off('mousemove', handleDrag).off('mouseup', stopDrag);
+                };
+            dragStart = getEvent();
+            $el = $(this);
+            $el.on('selectstart', preventSelection);
+            $('body').css('user-select', 'none');
+            $(window).on('mousemove', handleDrag).on('mouseup', stopDrag);
+        }
+    });
+}(jQuery));
